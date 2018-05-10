6a72479fd48b737a579ed85c2bdf03349662a298
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-10 09:43:32 -0600

    Inital commit. Write test and functionality for creating rectangles.

---
+++ rectangle.js
@@ -0,0 +1,9 @@
+function Rectangle(leftX, bottomY, width, height) {
+  this.leftX = leftX
+  this.bottomY = bottomY
+  this.width = width
+  this.height = height
+  }
+
+
+module.exports = Rectangle
---
+++ test/rectangle_test.js
@@ -0,0 +1,27 @@
+assert = require('chai').assert;
+const Rectangle = require('../rectangle')
+// const findLove = require('../rectangle').findLove
+
+describe("Rectangle", function(){
+  const rectangle1 = new Rectangle(1, 2, 6, 3)
+  const rectangle2 = new Rectangle(4, 2, 6, 3)
+  describe("attributes", function() {
+    it("can create 2 rectangles", function() {
+
+      assert.equal(rectangle1.leftX, 1);
+      assert.equal(rectangle1.bottomY, 2);
+      assert.equal(rectangle1.width, 6);
+      assert.equal(rectangle1.height, 3);
+      assert.equal(rectangle2.leftX, 4);
+      assert.equal(rectangle2.bottomY, 2);
+      assert.equal(rectangle2.width, 6);
+      assert.equal(rectangle2.height, 3);
+    })
+  })
+
+  describe("methods", function() {
+    it("can find the intersection of 2 rectangles", function() {
+      
+    })
+  })
+})
