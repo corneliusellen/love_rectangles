73fed8d2b6404154f135149c29893522a0e1c5b9
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-10 10:09:53 -0600

    Write functionality to find BottomY for rectangle 3. Passing test.

--- rectangle.js
+++ rectangle.js
@@ -5,13 +5,22 @@ function Rectangle(leftX, bottomY, width, height) {
   this.height = height
   }
 
-Rectangle.prototype.findLove = function(rectangle2) {
+Rectangle.prototype.findLeftX = function(rectangle2) {
   if (this.leftX < rectangle2.leftX) {
     var rectangle3LeftX = rectangle2.leftX
   } else {
     var rectangle3LeftX = this.leftX
   }
   return rectangle3LeftX
+}
+
+Rectangle.prototype.findBottomY = function(rectangle2) {
+  if (this.bottomY < rectangle2.bottomY) {
+    var rectangle3BottomY = rectangle2.bottomY
+  } else {
+    var rectangle3BottomY = this.bottomY
+  }
+  return rectangle3BottomY
 }
 
 module.exports = Rectangle
