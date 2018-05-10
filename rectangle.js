7738bf645ec58700e31bbf06ed4aff8bcbbe5cdb
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-10 10:04:39 -0600

    Write functionality for finding leftC of rectangle 3. Passing test.

--- rectangle.js
+++ rectangle.js
@@ -5,5 +5,13 @@ function Rectangle(leftX, bottomY, width, height) {
   this.height = height
   }
 
+Rectangle.prototype.findLove = function(rectangle2) {
+  if (this.leftX < rectangle2.leftX) {
+    var rectangle3LeftX = rectangle2.leftX
+  } else {
+    var rectangle3LeftX = this.leftX
+  }
+  return rectangle3LeftX
+}
 
 module.exports = Rectangle
